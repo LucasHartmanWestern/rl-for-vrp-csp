c20e1b33fbd4d50cf4fef17c7f7d27f0d83782c6
LucasHartman <lucas.hartman@kognitive.ca>
2024-10-22 16:42:31 -0400

    Change to 3 zones

--- experiments/Exp_47/train_job.sh
+++ experiments/Exp_47/train_job.sh
@@ -3,8 +3,8 @@
 #SBATCH --output=experiments/Exp_47/output.log
 #SBATCH --error=experiments/Exp_47/error.log
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=10
-#SBATCH --gpus-per-node=5
+#SBATCH --cpus-per-task=6
+#SBATCH --gpus-per-node=3
 #SBATCH --time=40:00:00
 #SBATCH --mem=64G
 
@@ -14,6 +14,6 @@ module load python/3.10 cuda cudnn
 source ~/envs/merl_env/bin/activate
 
 # Enable multi-threading
-export OMP_NUM_THREADS=10
+export OMP_NUM_THREADS=6
 
-python app_v2.py -g 0 1 2 3 4 -e 47 -d "/home/hartman/scratch/metrics/Exp"
+python app_v2.py -g 0 1 2 -e 47 -d "/home/hartman/scratch/metrics/Exp"
