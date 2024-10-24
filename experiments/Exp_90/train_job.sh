331d95b69d49db17a9f944fd6e8e13a8cc6bd7e9
E3than <epigou@uwo.ca>
2024-10-24 14:16:07 -0400

    exp_90 job file update

--- experiments/Exp_90/train_job.sh
+++ experiments/Exp_90/train_job.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-#SBATCH --job-name=Exp_7
-#SBATCH --output=experiments/Exp_7/output.log
-#SBATCH --error=experiments/Exp_7/error.log
+#SBATCH --job-name=Exp_90
+#SBATCH --output=experiments/Exp_90/output.log
+#SBATCH --error=experiments/Exp_90/error.log
 #SBATCH -A rrg-kgroling
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=6
-#SBATCH --gpus-per-node=3
+#SBATCH --gpus-per-node=4
 #SBATCH --time=15:00:00
 #SBATCH --mem=64G
 
@@ -17,4 +17,4 @@ source ~/envs/merl_env/bin/activate
 # Enable multi-threading
 export OMP_NUM_THREADS=6
 
-python app_v2.py -g 0 1 2 -e 7 -d "/home/hartman/scratch/metrics/Exp"
+python app_v2.py -g 0 1 2 -e 90 -d "/home/epigou/scratch/metrics/Exp"
