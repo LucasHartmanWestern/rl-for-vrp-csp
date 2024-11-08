e4f75b3bf2961fdbc7dbef15122a605367667f94
LucasHartman <lucas.hartman@kognitive.ca>
2024-11-08 17:52:13 -0500

    Add experiments for cpu only and big nn

---
+++ experiments/Exp_93/train_job.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#SBATCH --job-name=Exp_93_train
+#SBATCH --output=experiments/Exp_93/output.log
+#SBATCH --error=experiments/Exp_93/error.log
+#SBATCH -A rrg-kgroling
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=5
+#SBATCH --time=41:40:00
+#SBATCH --mem=24G
+#SBATCH --gpus-per-node=1
+
+echo "Starting training for experiment 93"
+
+set -e  # Exit immediately if a command exits with a non-zero status
+
+module load python/3.10 cuda cudnn
+source ~/envs/merl_env/bin/activate
+
+# Enable multi-threading
+export OMP_NUM_THREADS=2
+
+python app_v2.py -g 0 -e 93 -d "/home/hartman/scratch/metrics/Exp" 
+    
