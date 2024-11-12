842b0b350f3b788ace14fc8af140d60b15df53a9
LucasHartman <lucas.hartman@kognitive.ca>
2024-11-12 13:00:32 -0500

    Add discount factor fine tuning with PPO

---
+++ experiments/Exp_109/train_job.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#SBATCH --job-name=Exp_109_train
+#SBATCH --output=experiments/Exp_109/output.log
+#SBATCH --error=experiments/Exp_109/error.log
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=5
+#SBATCH --time=32:00:00
+#SBATCH --mem=24G
+
+echo "Starting training for experiment 109"
+
+set -e  # Exit immediately if a command exits with a non-zero status
+
+module load python/3.10 cuda cudnn
+source ~/envs/merl_env/bin/activate
+
+# Enable multi-threading
+export OMP_NUM_THREADS=2
+
+python app_v2.py -e 109 -d "/home/hartman/scratch/metrics/Exp" 
+    
