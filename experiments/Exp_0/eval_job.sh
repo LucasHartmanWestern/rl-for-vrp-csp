f6ff6cc0b25409962d96ecdddea936fb7c0d248f
LucasHartmanWestern <lhartma8@uwo.ca>
2024-11-21 12:14:24 -0500

    Add toy experiments

--- experiments/Exp_0/eval_job.sh
+++ experiments/Exp_0/eval_job.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
-#SBATCH --job-name=Exp_0_eval
-#SBATCH --output=experiments/Exp_0/output.log
-#SBATCH --error=experiments/Exp_0/error.log
+#SBATCH --job-name=Exp_1_eval
+#SBATCH --output=experiments/Exp_1/output.log
+#SBATCH --error=experiments/Exp_1/error.log
 #SBATCH -A def-mcapretz
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=3
-#SBATCH --time=00:22:30
+#SBATCH --cpus-per-task=5
+#SBATCH --time=41:40:00
 #SBATCH --mem=24G
 
 
-echo "Starting evaluation for experiment 0"
+echo "Starting evaluation for experiment 1"
 
 set -e  # Exit immediately if a command exits with a non-zero status
 
@@ -19,5 +19,5 @@ source ~/envs/merl_env/bin/activate
 # Enable multi-threading
 export OMP_NUM_THREADS=2
 
-python app_v2.py   -e 0 -d "/home/hartman/scratch/metrics/Exp" -eval True
+python app_v2.py   -e 1 -d "/home/hartman/scratch/metrics/Exp" -eval True
     
--- experiments/Exp_0/train_job.sh
+++ experiments/Exp_0/train_job.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
-#SBATCH --job-name=Exp_0_train
-#SBATCH --output=experiments/Exp_0/output.log
-#SBATCH --error=experiments/Exp_0/error.log
+#SBATCH --job-name=Exp_1_train
+#SBATCH --output=experiments/Exp_1/output.log
+#SBATCH --error=experiments/Exp_1/error.log
 #SBATCH -A def-mcapretz
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=3
-#SBATCH --time=00:22:30
+#SBATCH --cpus-per-task=5
+#SBATCH --time=41:40:00
 #SBATCH --mem=24G
 
 
-echo "Starting training for experiment 0"
+echo "Starting training for experiment 1"
 
 set -e  # Exit immediately if a command exits with a non-zero status
 
@@ -19,5 +19,5 @@ source ~/envs/merl_env/bin/activate
 # Enable multi-threading
 export OMP_NUM_THREADS=2
 
-python app_v2.py   -e 0 -d "/home/hartman/scratch/metrics/Exp" 
+python app_v2.py   -e 1 -d "/home/hartman/scratch/metrics/Exp" 
     
