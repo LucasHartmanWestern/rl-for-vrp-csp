79507f84846f801c964b74d343d0f30551ace3c9
Lucas Hartman <90533367+LucasHartmanWestern@users.noreply.github.com>
2025-03-01 17:49:05 -0500

    Add spring and autumn experiments

---
+++ experiments/Exp_7063/train_job.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#SBATCH --job-name=Exp_7063_train
+#SBATCH --output=experiments/Exp_7063/output.log
+#SBATCH --error=experiments/Exp_7063/error.log
+#SBATCH -A rrg-kgroling
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=5
+#SBATCH --time=133:00:00
+#SBATCH --mem=32G
+#SBATCH --gpus-per-node=1
+
+#SBATCH --mail-type=FAIL,TIME_LIMIT
+#SBATCH --mail-user=lhartma8@uwo.ca
+
+echo "Starting training for experiment 7063"
+
+set -e  # Exit immediately if a command exits with a non-zero status
+
+module load python/3.10 cuda cudnn
+source ~/envs/merl_env/bin/activate
+
+# Enable multi-threading
+export OMP_NUM_THREADS=2
+
+python app_v2.py -g 0 -e 7063 -d "/home/hartman/scratch/metrics/Exp" 
+    
---
+++ experiments/Exp_9030/train_job.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#SBATCH --job-name=Exp_9030_train
+#SBATCH --output=experiments/Exp_9030/output.log
+#SBATCH --error=experiments/Exp_9030/error.log
+#SBATCH -A def-mcapretz
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=5
+#SBATCH --time=90:00:00
+#SBATCH --mem=32G
+
+
+#SBATCH --mail-type=FAIL,TIME_LIMIT
+#SBATCH --mail-user=lhartma8@uwo.ca
+
+echo "Starting training for experiment 9030"
+
+set -e  # Exit immediately if a command exits with a non-zero status
+
+module load python/3.10 cuda cudnn
+source ~/envs/merl_env/bin/activate
+
+# Enable multi-threading
+export OMP_NUM_THREADS=2
+
+python app_v2.py  -e 9030 -d "/home/hartman/scratch/metrics/Exp" 
+    
