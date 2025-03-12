b6fcac5b49e7b6e57d166740e6f8f1ce1e372b2e
E3than <epigou@gmail.com>
2025-03-12 10:14:29 -0400

    fix

--- training_processes/train_odt.py
+++ training_processes/train_odt.py
@@ -209,7 +209,7 @@ class Experiment:
 
     def _load_dataset(self, env_name):
         print(f'Loading Dataset for Zone {self.zone_index}...')
-        
+        adjusted_experiment_number = str(int(self.experiment_number) - 108)
         # Path to the zone-specific PKL file
         #dataset_path = os.path.join(self.metrics_base_path, f'zone_{self.zone_index}.pkl')
         base_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '../..'))
