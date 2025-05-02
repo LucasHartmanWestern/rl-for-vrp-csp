8d7e3dfdc1272c31583ec6b437e7f304415eee6d
Lucas Hartman <90533367+LucasHartmanWestern@users.noreply.github.com>
2025-05-02 19:41:39 -0400

    Re-order per-zone config

---
+++ experiments/Exp_7076/train_job.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#SBATCH --job-name=Exp_7076_train
+#SBATCH --output=experiments/Exp_7076/output.log
+#SBATCH --error=experiments/Exp_7076/error.log
+#SBATCH -A def-mcapretz
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=5
+#SBATCH --time=10:40:00
+#SBATCH --mem=6G
+
+
+echo "Starting training for experiment 7076"
+
+set -e  # Exit immediately if a command exits with a non-zero status
+
+module load python/3.10 cuda cudnn
+source ~/envs/merl_env/bin/activate
+
+# Enable multi-threading
+export OMP_NUM_THREADS=2
+
+python app_v2.py  -e 7076 -d "/home/sgomezro/scratch/metrics/Exp" 
+    
