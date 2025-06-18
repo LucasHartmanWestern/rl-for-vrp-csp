b105c6157f1cf783864145baaac7b67f1d52fd8b
Santiago <sgomezro@uwo.ca>
2025-06-18 10:51:58 -0400

    changes on 4075 to run tests

--- experiments/Exp_4075/train_job.sh
+++ experiments/Exp_4075/train_job.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 #SBATCH --job-name=Exp_4075_train
-#SBATCH --output=experiments/Exp_4075/output.log
-#SBATCH --error=experiments/Exp_4075/error.log
+#SBATCH --output=experiments/Exp_4075/output_v4.log
+#SBATCH --error=experiments/Exp_4075/error_v4.log
 #SBATCH -A def-mcapretz
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=5
-#SBATCH --time=10:40:00
-#SBATCH --mem=6G
+#SBATCH --time=20:00:00
+#SBATCH --mem=12G
 
 
 echo "Starting training for experiment 4075"
@@ -19,5 +19,5 @@ source ~/envs/merl_env/bin/activate
 # Enable multi-threading
 export OMP_NUM_THREADS=2
 
-python app_v2.py  -e 4075 -d "/home/sgomezro/scratch/metrics/Exp" 
+python app_v2.py  -e 4075 -d "/home/sgomezro/scratch/metrics/Exp" -verb True
     
